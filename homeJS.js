7c3833f35f02add355b896e4d853ef6292588f8e
Badro <110125400+Badro4@users.noreply.github.com>
2025-01-25 16:59:02 +0100

    Delete homeJS.js

--- homeJS.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("clickable-iroha").addEventListener("click", function () {
-    const sound = document.getElementById("iroha-click")
-    sound.volume = 0.2
-    sound.play()
-})
